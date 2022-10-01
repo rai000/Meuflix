e51ed744fda2f2727349ced65ee0f4b196776c30
Railson <railson222@protonmail.com>
2022-10-01 16:17:22 -0300

    ola

---
+++ Aluraflix0/script.js
@@ -0,0 +1,6 @@
+var listaFilmes = ["https://www.sonypictures.com.br/sites/brazil/files/2022-04/UNCHARTED%20KEY%20ART.JPG", "https://i.pinimg.com/736x/4d/d2/3b/4dd23b8120032cce4e9826f3cbffeafc.jpg", "https://i.pinimg.com/736x/8e/be/b6/8ebeb60752f5f35a9937283f779f97b9--rapunzel-movie-rapunzel-hair.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++) {
+  
+  document.write("<img src=" + listaFilmes[i] + ">")
+} 
